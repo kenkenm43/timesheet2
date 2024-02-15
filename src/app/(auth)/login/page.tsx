092e25d9157cdd95e36b8de99fa65741a32ebfc6
kenkenm43 <kenkenm43@gmail.com>
2024-02-15 17:25:31 +0700

    first commit

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { authenticate } from "@/lib/actions";
+
+const Login = () => {
+  return (
+    <form action={authenticate}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
---
+++ src/components/auth/login-button.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "model" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginButton = {
+  chlidren,
+  mode,
+};
